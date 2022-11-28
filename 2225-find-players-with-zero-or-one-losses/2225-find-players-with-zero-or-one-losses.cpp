60b131dbbda4686dbbdb6d8fdfbdd64daa17f809
Harshit Pawar <97798536+hp222015@users.noreply.github.com>
2022-11-28 20:20:54 +0530

    Time: 1373 ms (48.11%), Space: 182 MB (40.02%) - LeetHub

--- 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.cpp
+++ 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.cpp
@@ -1,34 +1,28 @@
 class Solution {
 public:
     vector<vector<int>> findWinners(vector<vector<int>>& matches) {
-        unordered_set<int> zeroLoss, oneLoss, moreLoss;
-        
-        for(auto& match: matches)// match is a vector  that will contain [1,3] and further match.
+        unordered_set<int> seen;
+        unordered_map<int,int> lossesCount;
+        for(auto& match: matches)
         {
-            int winner= match[0], loser=match[1];
-            if((oneLoss.find(winner)==oneLoss.end()) && (moreLoss.find(winner)==moreLoss.end()))
-                zeroLoss.insert(winner);
-            if(zeroLoss.find(loser) !=zeroLoss.end())
-            {
-                zeroLoss.erase(loser);
-                oneLoss.insert(loser);
-            }
-            else if(oneLoss.find(loser) !=oneLoss.end())
-            {
-                oneLoss.erase(loser);
-                moreLoss.insert(loser);
-            }
-            else if(moreLoss.find(loser) !=moreLoss.end())
-                continue;
-            else
-                oneLoss.insert(loser);
+            int winner=match[0],loser=match[1];
+            seen.insert(winner);
+            seen.insert(loser);
+            lossesCount[loser]++;
         }
-       vector<vector<int>> answer(2, vector<int>());
-        answer[0].assign(zeroLoss.begin(),zeroLoss.end());
-        answer[1].assign(oneLoss.begin(),oneLoss.end());
+        vector<vector<int>>answer(2,vector<int>());
+        for(auto player: seen)
+        {
+           if(lossesCount.find(player) == lossesCount.end())
+               answer[0].push_back(player);
+           else if(lossesCount[player]==1)
+               answer[1].push_back(player);
+                
+        }
         sort(answer[0].begin(),answer[0].end());
         sort(answer[1].begin(),answer[1].end());
         
         return answer;
+        
     }
 };
