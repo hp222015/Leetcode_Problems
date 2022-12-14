2b2cb28c381c857728b3c46b40a55979ce7e94fc
Harshit Pawar <97798536+hp222015@users.noreply.github.com>
2022-12-14 11:44:35 +0530

    Time: 554 ms (5.02%), Space: 59.1 MB (37.50%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.cpp
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -2,11 +2,10 @@ class Solution {
 public:
     int maxArea(vector<int>& height) {
         // Approach : take two pointer first to left and the other to right and start finding area between the two.
-        int width=0;
         int area=0;
-        int maxi=INT_MIN;
+        int maxi=0;
         int left=0,right=height.size()-1;
-        while(left<=right)
+        while(left<right)
         {
             area=min(height[left],height[right])*(right-left);
             cout<<area<<" ";
