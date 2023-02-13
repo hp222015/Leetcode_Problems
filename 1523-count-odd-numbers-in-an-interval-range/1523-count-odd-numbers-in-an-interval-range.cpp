3446ff92da526540bfd8086c24be04ebe01dffbd
Harshit Pawar <97798536+hp222015@users.noreply.github.com>
2023-02-13 12:04:53 +0530

    Time: 3 ms (66.30%), Space: 5.9 MB (63.61%) - LeetHub

--- 1523-count-odd-numbers-in-an-interval-range/1523-count-odd-numbers-in-an-interval-range.cpp
+++ 1523-count-odd-numbers-in-an-interval-range/1523-count-odd-numbers-in-an-interval-range.cpp
@@ -1,13 +1,12 @@
 class Solution {
 public:
     int countOdds(int low, int high) {
-        int count=0;
-      while(low<=high)
-      {
-          if(low%2!=0)
-              count++;
-          low++;
-      }
-        return count;
+        /* to solve above problem in O(1) complexity
+            I need to find odd no from 1 to low-1 and odd no. from 1 to high
+            odd no. from 1 to any no. is (no.+1)/2
+            low-1 because we don't need to include low in count of odd no's from 1 to low
+            */
+        return ((high+1)/2-low/2);
+        
     }
 };
