8499d6b0d9cade5b46345954cf99e3110ea80c7e
Harshit Pawar <97798536+hp222015@users.noreply.github.com>
2023-03-13 11:19:01 +0530

    Time: 13 ms (52.01%), Space: 7.3 MB (84.57%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -3,17 +3,17 @@ public:
     bool isAnagram(string s, string t) {
         if(s.size()!=t.size())
             return false;
-        map<char,int>m1,m2;
+        map<char,int>m1;
         for(int i=0;i<s.size();i++)
         {
             m1[s[i]]++;
-            m2[t[i]]++;
+            m1[t[i]]--;
         }
-        for(auto i:m1)
-        {
-            if(m1[i.first]!=m2[i.first])
-                return false;
-        }
+       for(auto i:m1)
+       {
+           if(i.second!=0)
+               return false;
+       }
        return true;     
     }
 };
