baf5c4db4ff1af58ba6d2ecc780b8bc150a29119
Harshit Pawar <97798536+hp222015@users.noreply.github.com>
2023-03-22 14:35:39 +0530

    Time: 2 ms (52.32%), Space: 6 MB (12.20%) - LeetHub

--- 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.cpp
+++ 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.cpp
@@ -1,12 +1,10 @@
 class Solution {
 public:
     int trailingZeroes(int n) {
-        int sum=0;
-        while(n!=0)
-        {
-            sum+=n/5;
-            n=n/5;
-        }
-       return sum; 
+        if(n==0)
+            return 0;
+        else
+            return n/5+trailingZeroes(n/5);
+        
     }
 };
