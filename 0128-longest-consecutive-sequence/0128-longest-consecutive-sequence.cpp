3e7e61b65c6aaa120e0a40e28f80608dcf93d321
Harshit Pawar <97798536+hp222015@users.noreply.github.com>
2023-04-01 11:47:33 +0530

    Time: 144 ms (68.22%), Space: 49.8 MB (47.77%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -4,21 +4,20 @@ public:
         unordered_set<int>s;
         for(auto i:nums)
             s.insert(i);
-        int count=0,max=0;
+        int max=0;
         for(auto i:s)
         {
+            int count=0;
             int curr_elem=i,prev_elem=i-1;
             if(s.find(prev_elem)!=s.end())
                 continue;
-                
             while(s.find(curr_elem)!=s.end())
             {
-                curr_elem+=1;
+                curr_elem=curr_elem+1;
                 count++;
             }
             if(count>max)
                 max=count;
-            count=0;
         }
        return max; 
     }
