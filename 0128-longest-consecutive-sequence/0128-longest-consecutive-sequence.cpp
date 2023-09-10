b65c90266655ab3c82baf5ae12549abbddf69d40
Harshit Pawar <97798536+hp222015@users.noreply.github.com>
2023-09-10 18:18:57 +0530

    Time: 79 ms (81.71%), Space: 44.2 MB (74.71%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -4,13 +4,8 @@ public:
         sort(nums.begin(),nums.end());
         if(nums.size()==0)
             return 0;
-        for(int i=0;i<nums.size()-1;i++)
-        {
-            if(nums[i+1]==nums[i])
-                nums.erase(find(nums.begin(),nums.end(),nums[i--]));
-        }
-        for(auto i:nums)
-            cout<<i<<',';
+        auto it=unique(nums.begin(),nums.end());
+        nums.erase(it,nums.end());
         int count=1;
         vector<int> ans;
         for(int i=0;i<nums.size()-1;i++)
