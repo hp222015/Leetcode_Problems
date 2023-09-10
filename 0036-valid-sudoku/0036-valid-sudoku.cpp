b70c6fe47fb3223346c28a1f1724b58b96797c1e
Harshit Pawar <97798536+hp222015@users.noreply.github.com>
2023-09-10 15:52:28 +0530

    Time: 20 ms (54.56%), Space: 19.2 MB (53.37%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -1,28 +1,64 @@
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
-        
-std::unordered_set<char> rows[9];
-    std::unordered_set<char> cols[9];
-    std::unordered_set<char> boxes[9];
-    
-    for (int i = 0; i < 9; i++) {
-        for (int j = 0; j < 9; j++) {
-            char num = board[i][j];
-            if (num != '.') {
-                // Check if the number is already in the row, column, or box
-                if (rows[i].count(num) || cols[j].count(num) || boxes[(i/3)*3 + j/3].count(num)) {
-                    return false;
-                }
-                
-                // Add the number to the row, column, and box
-                rows[i].insert(num);
-                cols[j].insert(num);
-                boxes[(i/3)*3 + j/3].insert(num);
+        unordered_map<char,int>m1;
+        for(int i=0;i<9;i++)
+        {
+            for(int j=0;j<9;j++)
+            {
+               m1[board[i][j]]++;
             }
+            m1.erase('.');
+            for(auto k:m1)
+            {
+                if(k.second>1)
+                    return false;
+            }
+            m1.clear();
         }
-    }
-    
-    return true;
+        m1.clear();
+        for(int i=0;i<9;i++)
+        {
+            for(int j=0;j<9;j++)
+            {
+               m1[board[j][i]]++;
+            }
+            m1.erase('.');
+            for(auto k:m1)
+            {
+                if(k.second>1)
+                    return false;
+            }
+            m1.clear();
+        }
+        m1.clear();
+        int m=1,n=1,p=0,q=0,x=3,y=3;
+        while(y--)
+        {
+            while(x--)
+            {
+                for(int i=p;i<3*m;i++)
+                {
+                    for(int j=q;j<3*n;j++)
+                    {
+                        m1[board[i][j]]++;
+                    }                    
+                }
+                m1.erase('.');
+                for(auto k:m1)
+                    {
+                        if(k.second>1)
+                            return false;
+                    }
+                m1.clear();
+                n++;
+                q+=3; 
+            }
+            m++;
+            p+=3;
+            n=1;q=0;x=3;
+            m1.clear();
+        }
+        return true;
     }
 };
